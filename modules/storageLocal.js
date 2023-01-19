c7800bd920228651e6aaa1ddb78c4d88646e4dc1
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-01-18 20:07:10 -0500

    Modified list structure

--- modules/storageLocal.js
+++ modules/storageLocal.js
@@ -1,6 +1,15 @@
+const todosArray = [
+  {
+    index: 0, description: 'Learning html',
+  },
+  { index: 1, description: 'Learning css' },
+  { index: 2, description: 'Ruby' },
+];
+
 export default class StorageLocal {
   constructor() {
-
+    this.index = this.getTodosFromLocal().length + 1;
+    todosArray.forEach((todo) => this.addTodo(todo));
   }
 
     getTodosFromLocal = () => {
@@ -11,6 +20,19 @@ export default class StorageLocal {
       }
 
       return this.todos;
+    }
+
+    addTodo = (todo) => {
+      const newTodo = {
+        description: todo.description,
+        completed: todo.completed,
+        index: this.index,
+      };
+
+      const todos = this.getTodosFromLocal();
+      todos.push(newTodo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+      this.index += 1;
     }
 }
 
